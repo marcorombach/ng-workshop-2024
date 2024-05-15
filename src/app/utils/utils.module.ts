9935877a35b38c71026d19c8a19b6a1273143f23
Marco Rombach <marco@marcorombach.de>
2024-05-15 15:43:55 +0200

    removed some imports from utils module

--- src/app/utils/utils.module.ts
+++ src/app/utils/utils.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { registerLocaleData } from '@angular/common';
-import localeDe from '@angular/common/locales/de';
-import localeDeExtra from '@angular/common/locales/extra/de';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { RatingComponent } from './rating/rating.component';
 import { FormsModule } from '@angular/forms';
-
-
-registerLocaleData(localeDe, 'de-DE', localeDeExtra);
 
 @NgModule({
   declarations: [
